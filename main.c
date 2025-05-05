de28202b26c3e77021adcdb1f89a69c8b9548639
Samuel Cevada <scevada@live.com.mx>
2025-05-05 01:57:41 -0600

    Se añadio timer y un sistema de victoria/derrota

--- main.c
+++ main.c
@@ -7,37 +7,72 @@
 #define WIDTH 35
 #define HEIGHT 15
 
+// Variable global para el tiempo restante
+int remainingTime = 0;
+int gameRunning;
+
 void initGame(char gameArea[HEIGHT][WIDTH], int *x, int *y, int numX, int remainingX);
 void printGame(char gameArea[HEIGHT][WIDTH], int remainingX);
 void setColor(int color);
 void gotoxy(int x, int y);
 extern void moveCharacter(int *x, int *y, char gameArea[HEIGHT][WIDTH], char input, int *remainingX);
 void selectDifficulty(int *numX, int *time);
+void printFinalMessage(int result);
+DWORD WINAPI timerThread(LPVOID lpParam);
 
 int main() {
     char gameArea[HEIGHT][WIDTH];
-    int x, y, numX, time;  
+    int x, y, numX, time, op;  
     char input;
 
-    selectDifficulty(&numX, &time);
-    int remainingX = numX;
-
-    system("cls"); 
-    initGame(gameArea, &x, &y, numX, remainingX);  
-
-    while (1) {   
-
-        if (_kbhit()) {  
-            input = _getch();
-            moveCharacter(&x, &y, gameArea, input, &remainingX);
-            system("cls");
-            printGame(gameArea, remainingX);
-            if (!remainingX){
-                printf("Juego terminado!");
+    do{
+        gameRunning = 1;
+        system("cls");
+        selectDifficulty(&numX, &time);
+        remainingTime = time;
+        int remainingX = numX; 
+    
+        system("cls"); 
+        initGame(gameArea, &x, &y, numX, remainingX);  
+    
+        // Iniciar el hilo del tiempo
+        HANDLE hTimerThread = CreateThread(NULL, 0, timerThread, NULL, 0, NULL);
+    
+        while (gameRunning) {   
+    
+            if (_kbhit()) {  
+                input = _getch();
+                moveCharacter(&x, &y, gameArea, input, &remainingX);
+                system("cls");
+                printGame(gameArea, remainingX);
+                if (!remainingX) {
+                    printFinalMessage(1);     
+                } 
+                else if (remainingTime <= 0) {
+                    printFinalMessage(0);
+                }
             }
         }
-    }
+    
+        // Esperar a que termine el hilo del temporizador
+        WaitForSingleObject(hTimerThread, INFINITE);
+        CloseHandle(hTimerThread);
+    
+        setColor(7);  // Restaurar color normal
+        printf("\n\nJuego terminado\n0. Salir\n1. Volver a iniciar\nSeleccion: ");
+        fflush(stdin);
+        scanf("%d", &op);
+    }while(op != 0);
 
+    return 0;
+}
+
+// Función para el hilo del temporizador
+DWORD WINAPI timerThread(LPVOID lpParam) {
+    while (remainingTime > 0 && gameRunning) {
+        Sleep(1000);  // Esperar 1 segundo
+        remainingTime--;
+    }
     return 0;
 }
 
@@ -85,6 +120,38 @@ void initGame(char gameArea[HEIGHT][WIDTH], int *x, int *y, int numX, int remain
     printGame(gameArea, remainingX);
 }
 
+void printFinalMessage(int result){
+    // Obtener el tamaño actual de la consola
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    int consoleWidth = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    int consoleHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    
+    // Calcular la posición de inicio para centrar el tablero
+    int startX = (consoleWidth - WIDTH) / 2;
+    int startY = (consoleHeight - HEIGHT) / 2;
+   
+    // Asegurar que no sea negativo
+    if (startX < 0) startX = 0;
+    if (startY < 0) startY = 0;
+    
+    int messageLenght = 20;
+    int xCord = startX + (WIDTH - messageLenght) / 2;
+    int yCord = startY + HEIGHT + 2;
+
+    gotoxy(xCord, yCord);
+    if (result){
+        setColor(10);
+        printf("Felicidades! Has ganado");
+    }else{
+        setColor(12);
+        printf("Se acabo el tiempo!");
+    }
+
+    Sleep(3000);
+    gameRunning = FALSE;
+}
+
 // Imprime el area de juego
 void printGame(char gameArea[HEIGHT][WIDTH], int remainingX) 
 { 
@@ -105,7 +172,7 @@ void printGame(char gameArea[HEIGHT][WIDTH], int remainingX)
     // Calculamos la posicion del titulo
     int titleLength = 27;
     int titleX = startX + (WIDTH - titleLength) / 2;
-    int titleY = startY - 4;    
+    int titleY = startY - 5;    
 
     // Imprimir titulo del juego
     gotoxy(titleX, titleY);
@@ -115,12 +182,17 @@ void printGame(char gameArea[HEIGHT][WIDTH], int remainingX)
     //Calculamos la posicion del contador de X restantes
     int counterLength = 23;
     int counterX = startX + (WIDTH - counterLength) / 2;
-    int counterY = startY - 2; 
+    int counterY = startY - 3; 
 
     // Imprimir contador de x
     gotoxy(counterX, counterY);
     setColor(3);
     printf("Objetivos restantes: %d", remainingX);
+
+    // Imprimir contador de tiempo
+    gotoxy(counterX, counterY + 1);
+    setColor(remainingTime <= 10 ? 12 : 3);  // Rojo si quedan 10 segundos o menos
+    printf("Tiempo restante: %d s", remainingTime);
 
     for (int i = 0; i < HEIGHT; i++) 
     {
